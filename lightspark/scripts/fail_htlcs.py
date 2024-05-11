3c07a378e8279a7a00682e0852439e720067bdad
Jeremy Klein <jklein24@gmail.com>
2024-05-10 19:22:05 -0700

    lint

--- lightspark/scripts/fail_htlcs.py
+++ lightspark/scripts/fail_htlcs.py
@@ -1,11 +1,11 @@
 # Copyright ©, 2022-present, Lightspark Group, Inc. - All Rights Reserved
 
-FAIL_HTLCS_MUTATION = f"""
-mutation FailHtlcs($invoice_id: ID!, $cancel_invoice: Boolean!) {{
-	fail_htlcs(input: {{ invoice_id: $invoice_id, cancel_invoice: $cancel_invoice }}) {{
-        invoice {{
+FAIL_HTLCS_MUTATION = """
+mutation FailHtlcs($invoice_id: ID!, $cancel_invoice: Boolean!) {
+	fail_htlcs(input: { invoice_id: $invoice_id, cancel_invoice: $cancel_invoice }) {
+        invoice {
             id
-        }}
-    }}
-}}
+        }
+    }
+}
 """
